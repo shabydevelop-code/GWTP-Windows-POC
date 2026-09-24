eec3d483fda91f36f3d0d0a648f0a452b2ae8ae4
shabydevelop-code <shaby.develop@gmail.com>
2026-09-24 18:36:31 +0300

    Initial WPF POC

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GWTP_Windows_POC;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
