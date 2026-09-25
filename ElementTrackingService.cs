057836154e91fe18c30f1b3768c0871c8a3a979c
shabydevelop-code <shaby.develop@gmail.com>
2026-09-25 13:51:52 +0300

    feat: track transient menu UI for overlay collision

--- ElementTrackingService.cs
+++ ElementTrackingService.cs
@@ -15,7 +15,7 @@ internal sealed class ElementTrackingService : IDisposable
     private WinEventDelegate? _winEventDelegate;
     private bool _disposed;
 
-    public event Action<Rect>? BoundsChanged;
+    public event Action<Rect, IReadOnlyCollection<Rect>>? BoundsChanged;
     public event Action? ElementTemporarilyHidden;
     public event Action? ElementUnavailable;
 
@@ -37,6 +37,17 @@ internal sealed class ElementTrackingService : IDisposable
 
         try
         {
+            Automation.AddAutomationEventHandler(
+                AutomationElement.MenuOpenedEvent,
+                AutomationElement.RootElement,
+                TreeScope.Subtree,
+                OnTransientUiChanged);
+            Automation.AddAutomationEventHandler(
+                AutomationElement.MenuClosedEvent,
+                AutomationElement.RootElement,
+                TreeScope.Subtree,
+                OnTransientUiChanged);
+
             Automation.AddAutomationPropertyChangedEventHandler(
                 _element,
                 TreeScope.Element,
@@ -65,6 +76,11 @@ internal sealed class ElementTrackingService : IDisposable
         }
 
         _healthTimer.Start();
+    }
+
+    private void OnTransientUiChanged(object sender, AutomationEventArgs e)
+    {
+        _dispatcher.BeginInvoke(RefreshBounds);
     }
 
     private void OnAutomationPropertyChanged(object sender, AutomationPropertyChangedEventArgs e)
@@ -110,7 +126,7 @@ internal sealed class ElementTrackingService : IDisposable
                 return;
             }
 
-            BoundsChanged?.Invoke(bounds);
+            BoundsChanged?.Invoke(bounds, GetTransientUiBounds());
         }
         catch (ElementNotAvailableException)
         {
@@ -120,6 +136,43 @@ internal sealed class ElementTrackingService : IDisposable
         {
             NotifyUnavailable();
         }
+    }
+
+    private IReadOnlyCollection<Rect> GetTransientUiBounds()
+    {
+        var bounds = new List<Rect>();
+
+        try
+        {
+            var menus = AutomationElement.RootElement.FindAll(
+                TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Menu));
+
+            foreach (AutomationElement menu in menus)
+            {
+                try
+                {
+                    if (menu.Current.IsOffscreen || menu.Current.ProcessId != _element.Current.ProcessId)
+                    {
+                        continue;
+                    }
+
+                    var menuBounds = menu.Current.BoundingRectangle;
+                    if (!menuBounds.IsEmpty && menuBounds.Width > 0 && menuBounds.Height > 0)
+                    {
+                        bounds.Add(menuBounds);
+                    }
+                }
+                catch (ElementNotAvailableException)
+                {
+                }
+            }
+        }
+        catch (ElementNotAvailableException)
+        {
+        }
+
+        return bounds;
     }
 
     private void NotifyUnavailable()
@@ -165,6 +218,14 @@ internal sealed class ElementTrackingService : IDisposable
 
         try
         {
+            Automation.RemoveAutomationEventHandler(
+                AutomationElement.MenuOpenedEvent,
+                AutomationElement.RootElement,
+                OnTransientUiChanged);
+            Automation.RemoveAutomationEventHandler(
+                AutomationElement.MenuClosedEvent,
+                AutomationElement.RootElement,
+                OnTransientUiChanged);
             Automation.RemoveAutomationPropertyChangedEventHandler(_element, OnAutomationPropertyChanged);
         }
         catch
