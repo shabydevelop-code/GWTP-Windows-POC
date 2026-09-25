52e53e0e0163b04c09cc99ad858aaaacef287856
shabydevelop-code <shaby.develop@gmail.com>
2026-09-25 13:51:57 +0300

    feat: reposition guidance around transient UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -114,13 +114,13 @@ public partial class MainWindow : Window
         _elementTracker.Start();
     }
 
-    private void OnTrackedElementBoundsChanged(Rect bounds)
+    private void OnTrackedElementBoundsChanged(Rect bounds, IReadOnlyCollection<Rect> blockedBounds)
     {
         _highlightWindow ??= new HighlightWindow();
         _highlightWindow.ShowAt(bounds);
 
         _guidanceWindow ??= new GuidanceWindow();
-        _guidanceWindow.ShowNear(bounds);
+        _guidanceWindow.ShowNear(bounds, blockedBounds);
     }
 
     private void OnTrackedElementTemporarilyHidden()
