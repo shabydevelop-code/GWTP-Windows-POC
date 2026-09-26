1438458452dc0c0ecb2839bdde28bc9ceb60be23
shabydevelop-code <shaby.develop@gmail.com>
2026-09-26 22:41:46 +0300

    feat: run Windows runtime as native messaging host

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,6 +7,8 @@ namespace GWTP_Windows_POC;
 /// </summary>
 public partial class App : System.Windows.Application
 {
+    private NativeMessagingHost? _nativeMessagingHost;
+
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
@@ -14,10 +16,48 @@ public partial class App : System.Windows.Application
         AppDomain.CurrentDomain.UnhandledException += (_, args) => DiagnosticLog.Write("UnhandledException", args.ExceptionObject as Exception);
         TaskScheduler.UnobservedTaskException += (_, args) => DiagnosticLog.Write("UnobservedTaskException", args.Exception);
         DiagnosticLog.Write("RuntimeStartup");
+
+        var nativeMessagingMode = e.Args.Any(arg =>
+            arg.StartsWith("chrome-extension://", StringComparison.OrdinalIgnoreCase) ||
+            arg.StartsWith("extension://", StringComparison.OrdinalIgnoreCase));
+
+        var mainWindow = new MainWindow();
+        MainWindow = mainWindow;
+
+        if (nativeMessagingMode)
+        {
+            mainWindow.Hide();
+            var host = new NativeMessagingHost(mainWindow);
+            _ = RunNativeMessagingAsync(host);
+        }
+        else
+        {
+            mainWindow.Show();
+        }
+    }
+
+    private async Task RunNativeMessagingAsync(NativeMessagingHost host)
+    {
+        _nativeMessagingHost = host;
+        try
+        {
+            await host.RunAsync();
+        }
+        catch (Exception ex)
+        {
+            DiagnosticLog.Write("NativeMessagingHostFailed", ex);
+        }
+        finally
+        {
+            host.Dispose();
+            _nativeMessagingHost = null;
+            Dispatcher.Invoke(Shutdown);
+        }
     }
 
     protected override void OnExit(ExitEventArgs e)
     {
+        _nativeMessagingHost?.Dispose();
         DiagnosticLog.Write("RuntimeExit");
         base.OnExit(e);
     }
