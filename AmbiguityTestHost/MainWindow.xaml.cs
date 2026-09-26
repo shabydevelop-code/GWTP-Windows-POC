992e336101069b4566846f06b8394991f7d646de
shabydevelop-code <shaby.develop@gmail.com>
2026-09-26 12:37:11 +0300

    test: add dynamic UIA laboratory scenarios

--- AmbiguityTestHost/MainWindow.xaml.cs
+++ AmbiguityTestHost/MainWindow.xaml.cs
@@ -4,5 +4,39 @@ namespace GWTP_Ambiguity_TestHost;
 
 public partial class MainWindow : Window
 {
-    public MainWindow() => InitializeComponent();
+    private int _dynamicNameVersion = 1;
+    private SecondTestWindow? _secondWindow;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Closed += (_, _) => _secondWindow?.Close();
+    }
+
+    private void ChangeDynamicName_Click(object sender, RoutedEventArgs e)
+    {
+        _dynamicNameVersion++;
+        DynamicNameButton.Content = $"Dynamic target {_dynamicNameVersion}";
+    }
+
+    private void ToggleDynamicTarget_Click(object sender, RoutedEventArgs e)
+    {
+        DynamicTargetButton.Visibility =
+            DynamicTargetButton.Visibility == Visibility.Visible
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+    }
+
+    private void OpenSecondWindow_Click(object sender, RoutedEventArgs e)
+    {
+        if (_secondWindow is { IsVisible: true })
+        {
+            _secondWindow.Activate();
+            return;
+        }
+
+        _secondWindow = new SecondTestWindow();
+        _secondWindow.Closed += (_, _) => _secondWindow = null;
+        _secondWindow.Show();
+    }
 }
